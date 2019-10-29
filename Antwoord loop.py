6d2875a6c5585402793bd998f9c857e1a0dcbc99
Cendur <57105463+DwarfNinja@users.noreply.github.com>
2019-10-29 12:10:57 +0100

    Create Antwoord loop.py

---
+++ Antwoord loop.py
@@ -0,0 +1,10 @@
+antwoord = input("Welke superheld is het?")
+#eerste vraag hoort hier (Daniel en Frank)
+
+while antwoord != #math.random variable superheld (Alihan)
+
+#Naomi's Code
+
+    if antwoord == #math.random variable superheld (Alihan)
+        break
+print("Gefeliciteerd, je antwoord is correct!")
