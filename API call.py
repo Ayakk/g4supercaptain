1066716f9eceb71cce097931249680c6e718b5eb
f2b3coded <52601855+f2b3coded@users.noreply.github.com>
2019-10-29 12:19:10 +0100

    Create API call.py
    Dit maakt verbinding met de server en haalt informatie op

---
+++ API call.py
@@ -0,0 +1,31 @@
+import http.client
+import json
+import hashlib
+import requests
+import time
+
+#Dit maakt verbinding met de Marvel server om informatie op te halen
+
+public_key = '4948dd2ad64f8a47c7e882325121c4b2'
+private_key = 'c8a4085bf1d97e3f1d3615f458986467ed40009a'
+timestamp = str(time.time()) # pakt huidige tijd
+hash_material = timestamp+private_key+public_key #zet de tijd, private key en public key als 1 string
+pre_hashed = hashlib.md5(hash_material.encode()) #bovenstaaande string converted to bytecode
+hashed = pre_hashed.hexdigest()# hier wordt de MD5 hash gegenereerd
+param = {"ts": timestamp,
+         'apikey': '4948dd2ad64f8a47c7e882325121c4b2',
+         "hash": hashed
+         }
+#hash - a md5 digest of the ts parameter, your private key and your public key (e.g. md5(ts+privateKey+publicKey)
+
+#voorbeeld link  http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150
+def getMarvelCharacters():
+    characters = []
+    url = 'https://gateway.marvel.com:443/v1/public/characters'
+    response = requests.get(url, params=param)
+    # voor controle
+    print(response.url)
+    print(response.json())
+
+
+print(getMarvelCharacters())
