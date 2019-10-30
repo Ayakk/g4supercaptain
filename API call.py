dff7baee9277ae4d28865205058aa19ffba7650d
f2b3coded <52601855+f2b3coded@users.noreply.github.com>
2019-10-30 11:29:22 +0100

    Update API call.py
    Added character name and character hint

--- API call.py
+++ API call.py
@@ -11,19 +11,18 @@ timestamp = str(time.time()) # pakt huidige tijd
 hash_material = timestamp+private_key+public_key #zet de tijd, private key en public key als 1 string
 pre_hashed = hashlib.md5(hash_material.encode()) #bovenstaaande string converted to bytecode
 hashed = pre_hashed.hexdigest()# hier wordt de MD5 hash gegenereerd
-character_random_interval = str(random.randint(2, 30)) #zorgt ervoor dat de set gekozen characters random blijven
-character_limit = "10"
+character_random_interval = str(random.randint(100, 500)) #zorgt ervoor dat de set gekozen characters random blijven
+character_limit = "100"
 param = {"ts": timestamp,
          'apikey': '4948dd2ad64f8a47c7e882325121c4b2',
          "hash": hashed,
          "limit": character_limit,
          "offset": character_random_interval
          }
+
 #hash - a md5 digest of the ts parameter, your private key and your public key (e.g. md5(ts+privateKey+publicKey)
 
-#voorbeeld link  http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150
-def getMarvelCharacters():
-    characters = []
+def getMarvelCharacters():#maakt verbinding met de marvel server en haalt een aantal superheroes op
     url = 'https://gateway.marvel.com:443/v1/public/characters'
     response = requests.get(url, params=param)
     response = response.json()
@@ -31,7 +30,23 @@ def getMarvelCharacters():
     # voor controle
     with open('characters.txt', 'w+') as f:
         f.write(text)
-    print(text)
 
+def getMarvelCharacterHint(): #pakt 1 random superheroe met de criteria "has description"
+    with open('characters.txt', 'r') as f:
+        text = json.load(f)
+    teller = 0
+    begin_character_lijst = []
+    einde_character_lijst = []
 
-print(getMarvelCharacters())
+    for i in text["data"]["results"]:
+        if text["data"]["results"][teller]["description"] != "":
+            begin_character_lijst.append(text["data"]["results"][teller])
+        teller += 1
+
+    einde_character_lijst.append(begin_character_lijst[random.randint(0, (len(begin_character_lijst) - 1))])
+    character_hint = einde_character_lijst[0]["description"]
+    character_name = einde_character_lijst[0]["name"]
+    print(einde_character_lijst)
+
+getMarvelCharacters()
+getMarvelCharacterHint()
