5290fa248b7ce11e5070df5b69e98d3a2c1ab08e
Ayakk <57104499+Ayakk@users.noreply.github.com>
2019-10-30 13:54:49 +0100

    Create gui.py
    de code voor de gui, werkt op het moment niet.

---
+++ gui.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter.ttk import *
+
+gui = Tk()
+gui.title("ReverseAkinator")
+gui.geometry("1000x750")
+
+punten = 25
+
+scoredict = {}
+
+
+def pakNaam():
+    username_entry = naaminput.get()
+    stringvoorlabel = "Uw naam is " + username_entry
+    labelusername = Label(gui,text=stringvoorlabel, font="Arial 12 bold")
+    labelusername.place(x=40, y=700)
+
+#GEEF JE NAAM
+naaminput = Entry(gui, text="Voer je username in ").place(x=500, y=500)
+naambutton = Button(gui, text="Voer in", command=pakNaam()).place(x=600, y=500)
+
+#MAAK DE WIDGETS
+score = Label(gui, text=punten, font="Arial 12").place(x=0, y=0)
+username = Label(gui, text="USERNAME", font="Arial 12 italic").place(x=0, y=40)
+hints = Label(gui, text="HINTLIJST:", font="Arial 12 bold").place(x=450, y=0)
+inhoudHintlijst = Label(gui, text="Inhoud", font="Arial 12 italic").place(x=450, y=50)
+
+
+#MAAK DE HINTKNOP
+hintbutton = Button(gui, text="HINT").place(x=900, y=700)
+
+gui.mainloop()
